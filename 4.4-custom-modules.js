ff7138d780065c3ff59ff72cd3736f59fcc915ee
mi-skam <maksim.codes@mailbox.org>
2022-03-22 22:10:51 +0100

    started with 4.4 custom modules

---
+++ 4.4-custom-modules.js
@@ -0,0 +1,9 @@
+const wordCount = require('./wordCount');
+
+const sentence = 'Wo viel Licht ist, ist auch Schatten.';
+const countedWords = wordCount(sentence);
+/* output */
+console.log(sentence);
+for (let i in countedWords) {
+  console.log(countedWords[i] + 'x' + i);
+}
---
+++ wordCount.js
@@ -0,0 +1,13 @@
+const ignore = /[\.,]/g;
+const separator = ' ';
+
+module.exports = function wordCount(sentence) {
+  return sentence
+    .replace(ignore, ' ')
+    .toLowerCase()
+    .split(separator)
+    .reduce((prev, current) => {
+      prev[current] += 1 ?? 1;
+      return prev;
+    });
+};
